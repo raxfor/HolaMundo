76a17131d16419a4d88d59af14c5744843240e6a
raxfor <jhonalexslb@gmail.com>
2022-10-08 12:34:27 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Introduce tu nombre");
+String nombre = Console.ReadLine();
+
+Console.WriteLine("Son las 10 am " + nombre);
+
+//Edad
+
+//Ciudad
+
+//Presentar mensaje "Hola, Nombre. Tienes Años. Vives en Ciudad.
+
+Console.WriteLine();
+
+
