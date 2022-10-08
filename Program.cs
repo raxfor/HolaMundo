efc488d47f84c46b1be84a0a9d5aecdc52e440ce
raxfor <jhonalexslb@gmail.com>
2022-10-08 12:44:37 -0500

    Ejercicio 1 y 2 del curso

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
-﻿Console.WriteLine("Introduce tu nombre");
+﻿/*
+ * Ejercicio1
+ *Escribe un programa que reciba tu nombre y lo escriba por consola
+ */
+Console.WriteLine("Introduce tu nombre: ");
 String nombre = Console.ReadLine();
 
-Console.WriteLine("Son las 10 am " + nombre);
+Console.WriteLine("Hola me llamo " + nombre);
 
 //Edad
 
 //Ciudad
 
 //Presentar mensaje "Hola, Nombre. Tienes Años. Vives en Ciudad.
+/*
+ * Ejercicio 2
+ * Escribe un programa que tome la hora y la escriba por pantalla.
+ */
 
-Console.WriteLine();
+Console.WriteLine("Ingresa la hora actual: ");
+String hora = Console.ReadLine();
+
+Console.WriteLine("La hora que ingresastes es " + hora);
+
 
 
